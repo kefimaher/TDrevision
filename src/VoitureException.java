efacea1cf22bcf242636522656430ab39bbdb785
Maher Kefi <kefi.maher@esprit.tn>
2023-11-03 11:42:33 +0100

    6

--- src/VoitureException.java
+++ src/VoitureException.java
@@ -1,17 +1,13 @@
 public class VoitureException extends Exception {
-
     public VoitureException() {
         super();
     }
-
     public VoitureException(String message) {
         super(message);
     }
-
     public VoitureException(String message, Throwable cause) {
         super(message, cause);
     }
-
     public VoitureException(Throwable cause) {
         super(cause);
     }
