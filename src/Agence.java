56558d078922fb7d656b06376171aaef44d36c31
Maher Kefi <kefi.maher@esprit.tn>
2023-11-03 11:28:50 +0100

    4

--- src/Agence.java
+++ src/Agence.java
@@ -1,42 +1,53 @@
-import java.util.Map;
+import java.util.*;
 
 public class Agence {
-    private String nom; private ListVoitures
-        vs; private Map<Client, ListVoitures>
-        ClientVoitureLoue; public Agence(String nom)
+    private String nom;
+    private ListVoitures vs;
+    private Map<Client, ListVoitures> ClientVoitureLoue;
+    public Agence(String nom)
 {
 //à completer
 }
-    public void ajoutVoiture(Voiture v)
-            throws VoitureException{
-// à completer
+    public void ajoutVoiture(Voiture v) throws VoitureException{
+        vs.ajoutVoiture(v);
     }
-    public void suppVoiture(Voiture
-                                    v)throws VoitureException{
-//à completer
+    public void suppVoiture(Voiture v)throws VoitureException{
+        vs.supprimeVoiture(v);
+        // Supprimer la voiture de la liste des voitures louées par les clients
+        for (ListVoitures listVoitures : ClientVoitureLoue.values()) {
+            listVoitures.supprimeVoiture(v);
+        }
     }
-    public void loueClientVoiture(Client cl,
-                                  Voiture
-                                          v)throws
-            VoitureException{
-// à completer
+    public void loueClientVoiture(Client cl, Voiture v)throws VoitureException
+    {
+        if (!ClientVoitureLoue.containsKey(cl)) {
+            ClientVoitureLoue.put(cl, new ListVoitures());
+        }
+        ListVoitures clientVoitures = ClientVoitureLoue.get(cl);
+        clientVoitures.ajoutVoiture(v);
     }
-    public void retourClientVoiture(Client
-                                            cl , Voiture v) throws
-            VoitureException{
-// à completer
+    public void retourClientVoiture(Client cl , Voiture v) throws VoitureException
+    {
+        if (ClientVoitureLoue.containsKey(cl)) {
+            ListVoitures clientVoitures = ClientVoitureLoue.get(cl);
+            clientVoitures.supprimeVoiture(v);
+        }
     }
-    public List<Voiture>
-    selectVoitureSelonCritere(Critere c){
-//à completer
+    public List<Voiture> selectVoitureSelonCritere(Critere c){
+        List<Voiture> voituresSatisfaisantes = new ArrayList<>();
+        for (Voiture voiture : vs.getVoitures()) {
+            if (c.estSatisfaitPar(voiture)) {
+                voituresSatisfaisantes.add(voiture);
+            }
+        }
+        return voituresSatisfaisantes;
     }
-    public Set<Client>
-    ensembleClientsLoueurs(){
-// à completer
+    public Set<Client> ensembleClientsLoueurs(){
+        return ClientVoitureLoue.keySet();
     }
-    public Collection<ListVoitures>
-    collectionVoituresLouees(){
-// à completer
+    public Collection<ListVoitures> collectionVoituresLouees()
+    {
+        return ClientVoitureLoue.values();
     }
     public void
     afficheLesClientsEtLeursListesVoitures(){
