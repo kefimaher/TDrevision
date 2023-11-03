d9331beb48cdc1a53f47e2f3c37c54fda1bd72ea
Maher Kefi <kefi.maher@esprit.tn>
2023-11-03 11:37:53 +0100

    5

--- src/Agence.java
+++ src/Agence.java
@@ -4,36 +4,38 @@ public class Agence {
     private String nom;
     private ListVoitures vs;
     private Map<Client, ListVoitures> ClientVoitureLoue;
-    public Agence(String nom)
-{
-//à completer
-}
-    public void ajoutVoiture(Voiture v) throws VoitureException{
+
+    public Agence(String nom, ListVoitures vs, Map<Client, ListVoitures> clientVoitureLoue) {
+        this.nom = nom;
+        this.vs = vs;
+        ClientVoitureLoue = clientVoitureLoue;
+    }
+
+    public void ajoutVoiture(Voiture v) throws VoitureException {
         vs.ajoutVoiture(v);
     }
-    public void suppVoiture(Voiture v)throws VoitureException{
+    public void suppVoiture(Voiture v) throws VoitureException {
         vs.supprimeVoiture(v);
         // Supprimer la voiture de la liste des voitures louées par les clients
         for (ListVoitures listVoitures : ClientVoitureLoue.values()) {
             listVoitures.supprimeVoiture(v);
         }
     }
-    public void loueClientVoiture(Client cl, Voiture v)throws VoitureException
-    {
+    public void loueClientVoiture(Client cl, Voiture v) throws VoitureException {
         if (!ClientVoitureLoue.containsKey(cl)) {
             ClientVoitureLoue.put(cl, new ListVoitures());
         }
         ListVoitures clientVoitures = ClientVoitureLoue.get(cl);
         clientVoitures.ajoutVoiture(v);
     }
-    public void retourClientVoiture(Client cl , Voiture v) throws VoitureException
-    {
+    public void retourClientVoiture(Client cl, Voiture v) throws VoitureException {
         if (ClientVoitureLoue.containsKey(cl)) {
             ListVoitures clientVoitures = ClientVoitureLoue.get(cl);
             clientVoitures.supprimeVoiture(v);
         }
     }
-    public List<Voiture> selectVoitureSelonCritere(Critere c){
+
+    public List<Voiture> selectVoitureSelonCritere(Critere c) {
         List<Voiture> voituresSatisfaisantes = new ArrayList<>();
         for (Voiture voiture : vs.getVoitures()) {
             if (c.estSatisfaitPar(voiture)) {
@@ -42,26 +44,32 @@ public class Agence {
         }
         return voituresSatisfaisantes;
     }
-    public Set<Client> ensembleClientsLoueurs(){
+
+    public Set<Client> ensembleClientsLoueurs() {
         return ClientVoitureLoue.keySet();
     }
-    public Collection<ListVoitures> collectionVoituresLouees()
-    {
+
+    public Collection<ListVoitures> collectionVoituresLouees() {
         return ClientVoitureLoue.values();
     }
-    public void
-    afficheLesClientsEtLeursListesVoitures(){
-//à compléter
-    }
-    public Map<Client, ListVoitures>
-    triCodeCroissant(){
 
-        6
-
-// à completer
+    public void afficheLesClientsEtLeursListesVoitures() {
+        for (Map.Entry<Client, ListVoitures> entry : ClientVoitureLoue.entrySet()) {
+            System.out.println("Client: " + entry.getKey());
+            System.out.println("Voitures loues: ");
+            entry.getValue().affiche();
+            System.out.println("_____");
+        }
     }
-    public Map<Client, ListVoitures>
-    triNomCroissant(){
-// à completer
+
+    public Map<Client, ListVoitures> triCodeCroissant() {
+        TreeMap<Client, ListVoitures> trieeParCode = new TreeMap<>(Comparator.comparingInt(Client::getCode));
+        trieeParCode.putAll(ClientVoitureLoue);
+        return trieeParCode;
+    }
+    public Map<Client, ListVoitures> triNomCroissant() {
+        TreeMap<Client, ListVoitures> trieeParNom = new TreeMap<>(Comparator.comparing(Client::getNom));
+        trieeParNom.putAll(ClientVoitureLoue);
+        return trieeParNom;
     }
 }
